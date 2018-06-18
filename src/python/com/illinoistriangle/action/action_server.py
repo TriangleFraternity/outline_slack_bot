52e984bf632921ef5b958c7476ab80f406eca837
Yi Cheng <wisechengyi@gmail.com>
2018-06-17 20:02:57 -0700

    separate handler

--- src/python/com/illinoistriangle/action/action_server.py
+++ src/python/com/illinoistriangle/action/action_server.py
@@ -9,10 +9,16 @@ app = Flask(__name__)
 
 
 @app.route('/action', methods=['GET', 'POST'])
-def handle_action():
+def flask_handle():
   payload = json.loads(request.form['payload'])
   app.logger.debug(payload)
 
+  handle_payload(payload)
+  app.logger.debug(r.status_code)
+  return 'Hello, World!'
+
+
+def handle_payload(payload):
   response_url = payload['response_url']
   trigger_user = payload['user']['name']
   text = payload['message']['text']
@@ -24,15 +30,23 @@ def handle_action():
   else:
     response_text = 'No url found.'
     response_type = 'ephemeral'
-
   reply_data = {
     "text": response_text,
     "response_type": response_type
   }
+  requests.post(response_url, json=reply_data)
 
-  r = requests.post(response_url, json=reply_data)
-  app.logger.debug(r.status_code)
-  return 'Hello, World!'
+
+# This is the entry point for aws lambda execution.
+def lambda_handler(data, context):
+  """Handle an incoming HTTP request from a Slack chat-bot.
+  """
+
+  print(data)
+  if "challenge" in data:
+    return data["challenge"]
+
+  handle_payload(json.loads(data))
 
 
 if __name__ == "__main__":
