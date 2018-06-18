3c385e33a7659a5dd6bcbefbc53a7fccb5fd180a
Yi Cheng <wisechengyi@gmail.com>
2018-06-18 10:32:21 +0800

    can reply with outline urls now

--- src/python/com/illinoistriangle/action/action_server.py
+++ src/python/com/illinoistriangle/action/action_server.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from com.illinoistriangle.lib.urlmarker import find_urls
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -10,10 +11,22 @@ def handle_action():
     app.logger.debug(payload)
 
     response_url = payload['response_url']
+    trigger_user = payload['user']['name']
+    text = payload['message']['text']
+    urls = find_urls(text)
+    if urls:
+        outline_urls = ['<https://outline.com/{}|{}...>'.format(u, u[:30]) for u in urls]
+        response_text = "Per @{}'s request, here are the outline urls: {}".format(trigger_user, ' '.join(outline_urls)) 
+        response_type = 'in_channel'
+    else:
+        response_text = 'No url found.'
+        response_type = 'ephemeral'
+
     reply_data = {
-            "text": "It's 80 degrees right now.",
-            "response_type": "in_channel"
+            "text": response_text,
+            "response_type": response_type
             }
+
     r = requests.post(response_url, json=reply_data)
     app.logger.debug(r.status_code)
     return 'Hello, World!'
