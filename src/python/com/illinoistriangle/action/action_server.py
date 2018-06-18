fc570fca10c76a5793d8e6b9c82bac2c4fa5b956
Yi Cheng <wisechengyi@gmail.com>
2018-06-18 10:06:00 +0800

    can respond to response_url

--- src/python/com/illinoistriangle/action/action_server.py
+++ src/python/com/illinoistriangle/action/action_server.py
@@ -1,8 +1,21 @@
-from flask import Flask
+import requests
+import json
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/action')
-def hello_world():
+
+@app.route('/action', methods=['GET', 'POST'])
+def handle_action():
+    payload = json.loads(request.form['payload'])
+    app.logger.debug(payload)
+
+    response_url = payload['response_url']
+    reply_data = {
+            "text": "It's 80 degrees right now.",
+            "response_type": "in_channel"
+            }
+    r = requests.post(response_url, json=reply_data)
+    app.logger.debug(r.status_code)
     return 'Hello, World!'
 
-app.run(port=443)
+app.run(debug=True, host='0.0.0.0', port=80)
